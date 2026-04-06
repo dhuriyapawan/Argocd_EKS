3b4b38a4cb5974e6817b29b7ae6dc3291f08942c
pawandhuriya@outlook.com <pawandhuriya@outlook.com>
2026-04-06 09:46:24 +0530

    First Test Command

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,60 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 # --- VPC ---
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = ">= 6.0.0"
+  version = "~> 6.0"
 
-  name                 = "my-vpc"
-  cidr                 = var.vpc_cidr
-  azs                  = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  public_subnets       = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
-  private_subnets      = ["10.0.11.0/24","10.0.12.0/24","10.0.13.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  name = "my-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets  = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+  private_subnets = ["10.0.11.0/24","10.0.12.0/24","10.0.13.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  # REQUIRED for EKS
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 # --- EKS Cluster ---
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "~>20.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
   cluster_name    = var.cluster_name
   cluster_version = "1.28"
-  subnet_ids      = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  # REQUIRED in v21
+  cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 2
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 2
+
       instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
     }
   }
 }
@@ -35,6 +63,7 @@ module "eks" {
 resource "aws_ecr_repository" "my_app" {
   name                 = "my-app"
   image_tag_mutability = "MUTABLE"
+
   encryption_configuration {
     encryption_type = "AES256"
   }
