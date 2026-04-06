07bba93eea6c6c6e0ffea9a1e160ed4c501c72e7
pawandhuriya@outlook.com <pawandhuriya@outlook.com>
2026-04-06 09:31:59 +0530

    First Test Command

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ module "vpc" {
 # --- EKS Cluster ---
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "20.0.0"
+  version         = "~>20.0"
   cluster_name    = var.cluster_name
   cluster_version = "1.28"
   subnet_ids      = module.vpc.private_subnets
